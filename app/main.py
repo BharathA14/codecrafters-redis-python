1d722627eb912239b2fe8b7b0bfacfa7dfc85a5e
Bharath A <bbharath201@gmail.com>
2025-07-05 23:08:48 +0530

    Respond to echo

--- app/main.py
+++ app/main.py
@@ -1,12 +1,31 @@
 import socket
 import threading
 
-def sendMessage(c):
-    #print("Entered")
-    while b := c.recv(4096):
-        if b=="":
-            break   
-        c.sendall(b"+PONG\r\n")
+def response_gen(decoded_data):
+    command = decoded_data[2].lower()
+
+    match command:
+        case "echo":
+            response = (f"{decoded_data[3]}\r\n{decoded_data[4]}\r\n").encode()
+        case _:
+            response = ("+PONG\r\n").encode()
+    return response
+
+
+def handle_request(client_socket: socket.socket):
+    try:
+        while True:
+            data = client_socket.recv(2048)
+            if not data:
+                break
+            decoded = data.decode().split("\r\n")
+
+            response = response_gen(decoded)
+            client_socket.sendall(response)
+
+    except Exception as e:
+        print("exception: ", e)
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -20,7 +39,7 @@ def main():
     while True:
         #print("New conn")
         c, _ = server_socket.accept()
-        threading.Thread(target=sendMessage, args=(c,)).start()
+        threading.Thread(target=handle_request, args=(c,)).start()
 
 if __name__ == "__main__":
     main()
