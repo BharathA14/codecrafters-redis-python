25579055e5edb52031fcbbaf4c61f085d13d1ef9
Bharath A <bbharath201@gmail.com>
2025-07-05 22:45:06 +0530

    Accept multiple conn

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
-import socket  # noqa: F401
+import socket
+import threading
 
+def sendMessage(c):
+    #print("Entered")
+    while b := c.recv(4096):
+        if b=="":
+            break   
+        c.sendall(b"+PONG\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,11 +15,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    c, _ = server_socket.accept()
+    #print("Server started")
 
     while True:
-        bytes, _ =  c.recvfrom(100)
-        c.sendall(b"+PONG\r\n")
+        #print("New conn")
+        c, _ = server_socket.accept()
+        threading.Thread(target=sendMessage, args=(c,)).start()
 
 if __name__ == "__main__":
     main()
