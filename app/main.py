b78b93380c37e1e1f24419e2ab5e7f1425254952
Bharath A <bbharath201@gmail.com>
2025-07-16 16:15:58 +0530

    Existing solution

--- app/main.py
+++ app/main.py
@@ -1,244 +1,218 @@
+import dataclasses
 import socket
 import threading
-from collections import defaultdict
-import time
+import datetime
 import argparse
-import app.rdb_parser as rdb_parser
-import re
+from typing import Any, Dict, Optional, cast, List
 
-parser = argparse.ArgumentParser()
-master_host = ""
-master_host_port = ""
-replica_connections = list()
-master_connection_socket = None
-master_replication_id = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
-master_replication_offset = "0"
+EMPTY_RDB = bytes.fromhex(
+    "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+)
 
 
-def response_gen(
-    client_socket: socket.socket,
-    raw_command: bytes,
-    decoded_data: list,
-    key_store: dict[str, list],
-    args: dict[str, str],
-    client_addr: str
-):
-    print("decoded in response gen:", decoded_data)
-    command = decoded_data[2].lower()
+@dataclasses.dataclass
+class Args:
+    port: int
+    replicaof: Optional[str]
 
-    match command:
-        case "echo":
-            client_socket.sendall(
-                f"{decoded_data[3]}\r\n{decoded_data[4]}\r\n".encode()
-            )
 
-        case "set":
-            time_out = 0
-            key = decoded_data[4]
-            value = decoded_data[6]
+def parse_next(data: bytes):
+    first, data = data.split(b"\r\n", 1)
+    match first[:1]:
+        case b"*":
+            value = []
+            l = int(first[1:].decode())
+            for _ in range(l):
+                item, data = parse_next(data)
+                value.append(item)
+            return value, data
+        case b"$":
+            l = int(first[1:].decode())
+            blk = data[:l]
+            data = data[l + 2 :]
+            return blk, data
 
-            if len(decoded_data) == 12:
-                time_out = time.time() + float(decoded_data[10]) / 1000
-            key_store[key] = [value, time_out]
+        case b"+":
+            return first[1:].decode(), data
 
-            if not is_replica():
-                handle_replica_set(raw_command)
-                client_socket.sendall("+OK\r\n".encode())
-            else:
-                if client_socket != master_connection_socket:
-                    client_socket.sendall("+OK\r\n".encode())
-
-        case "get":
-            if decoded_data[4] in key_store and (
-                key_store[decoded_data[4]][1] == 0
-                or (key_store[decoded_data[4]][1] >= time.time())
-            ):
-                # print(key_store[decoded_data[4]][1], time.time())
-                value = key_store[decoded_data[4]][0]
-                client_socket.sendall(f"${len(value)}\r\n{value}\r\n".encode())
-            else:
-                client_socket.sendall("$-1\r\n".encode())
-
-        case "config":
-            match decoded_data[6]:
-                case "dir":
-                    client_socket.sendall(generate_resp_array([decoded_data[6], args.dir]).encode())  # type: ignore
-                case "dbfilename":
-                    client_socket.sendall(generate_resp_array([decoded_data[6], args.dbfilename]).encode())  # type: ignore
-
-        case "keys":
-            matching_keys = []
-            pattern = re.escape("*")
-            for i in list(key_store.keys()):
-                if re.search(pattern, decoded_data[4]):
-                    matching_keys.append(i)
-            client_socket.sendall(generate_resp_array(matching_keys).encode())
-
-        case "info":
-            if args.replicaof == "":  # type: ignore
-                response = generate_bulk_string(
-                    "{"
-                    + "role:master,master_replid:"
-                    + master_replication_id
-                    + ",master_repl_offset:"
-                    + "0"
-                    + "}"
-                ).encode()
-            else:
-                response = generate_bulk_string("{role:slave}").encode()
-            client_socket.sendall(response)
-
-        case "replconf":
-            if decoded_data[4].lower() == "listening-port":
-                try:
-                    replica_port = int(decoded_data[6])
-                    print(f"Replica reported listening on port {replica_port}. Connecting...")
-
-                    propagation_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    propagation_socket.connect((client_addr, replica_port))
-                    
-                    replica_connections.append(propagation_socket)
-                    print(f"Successfully connected to replica at {client_addr}:{replica_port}. Connection stored.")
-
-                except Exception as e:
-                    print(f"Failed to connect to replica at {client_addr}:{replica_port}. Error: {e}")
-
-            client_socket.sendall(("+OK\r\n").encode())
-
-        case "psync":
-            add_wait_to_send_message(
-                client_socket,
-                generate_bulk_string(
-                    "FULLRESYNC "
-                    + master_replication_id
-                    + " "
-                    + master_replication_offset
-                ).encode(),
-            )
-            print(rdb_parser.send_rdb_file())
-            add_wait_to_send_message(client_socket, rdb_parser.send_rdb_file())
         case _:
-            client_socket.sendall(("+PONG\r\n").encode())
+            raise RuntimeError(f"Parse not implemented: {first[:1]}")
 
 
-def handle_replica_set(command: bytes):
-    global replica_connections
-    print(f"Propagating command to {len(replica_connections)} replica(s)...")
-    
-    failed_connections = []
+def encode_resp(data: Any, trailing_crlf: bool = True) -> bytes:
+    if isinstance(data, bytes):
+        return b"$%b\r\n%b%b" % (
+            str(len(data)).encode(),
+            data,
+            b"\r\n" if trailing_crlf else b"",
+        )
+    if isinstance(data, str):
+        return b"+%b\r\n" % (data.encode(),)
+    if data is None:
+        return b"$-1\r\n"
+    if isinstance(data, list):
+        return b"*%b\r\n%b" % (
+            str(len(data)).encode(),
+            b"".join(map(encode_resp, data)),
+        )
 
-    for replica_socket in replica_connections:
-        try:
-            replica_socket.sendall(command)
-        except (BrokenPipeError, OSError) as e:
-            print(f"Failed to propagate to replica {replica_socket.getpeername()}. Error: {e}. Removing connection.")
-            failed_connections.append(replica_socket)
-
-    if failed_connections:
-        replica_connections = [conn for conn in replica_connections if conn not in failed_connections]
-
-
-def generate_bulk_string(i: str):
-    return f"${len(i)}\r\n{i}\r\n"
-
-
-def generate_resp_array(data: list) -> str:
-    n = len(data)
-    response = [f"*{n}\r\n"]
-    for i in data:
-        response.append(f"${len(i)}\r\n{i}\r\n")
-    print(response)
-    return "".join(response)
+    raise RuntimeError(f"Encode not implemented: {data}")
 
 
-def handle_request(client_socket: socket.socket, key_store: defaultdict, args: dict, client_addr: str):
-    try:
-        while True:
-            data = client_socket.recv(2048)
-            if not data:
-                break
-            print("decoded data:", data.decode())
-            decoded = data.decode().split("\r\n")
-
-            response_gen(client_socket, data, decoded, key_store, args, client_addr)
-
-    except Exception as e:
-        print("exception: ", e)
+@dataclasses.dataclass
+class Value:
+    value: Any
+    expiry: Optional[datetime.datetime]
 
 
-def parse_master_config(config: str):
-    global master_host, master_host_port
-    if config != "":
-        master_host, master_host_port = config.split(" ")
-        master_host_port = int(master_host_port)
+db: Dict[Any, Value] = {}
 
 
-def is_replica():
-    # print("master_host",master_host)
-    return master_host != ""
+@dataclasses.dataclass
+class Replication:
+    master_replid: str
+    master_repl_offset: int
+    connected_replicas: List[socket.socket]
 
 
-def add_wait_to_send_message(s: socket.socket, message: bytes):
-    s.sendall(message)
-    time.sleep(0.1)
+replication = Replication(
+    master_replid="8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb",
+    master_repl_offset=0,
+    connected_replicas=[],
+)
 
 
-def ping_master(s: socket.socket):
-    add_wait_to_send_message(s, generate_resp_array(["PING"]).encode())
+def handle_conn(args: Args, conn: socket.socket, is_replica_conn: bool = False):
+    data = b""
+    while data or (data := conn.recv(4096)):
+        value, data = parse_next(data)
+        match value:
+            case [b"PING"]:
+                conn.send(encode_resp("PONG"))
+            case [b"ECHO", s]:
+                conn.send(encode_resp(s))
+            case [b"SET", k, v]:
+                for rep in replication.connected_replicas:
+                    rep.send(encode_resp(value))
+                db[k] = Value(
+                    value=v,
+                    expiry=None,
+                )
+                if not is_replica_conn:
+                    conn.send(encode_resp("OK"))
+            case [b"SET", k, v, b"px", expiry_ms]:
+                for rep in replication.connected_replicas:
+                    rep.send(encode_resp(value))
+                now = datetime.datetime.now()
+                expiry_ms = datetime.timedelta(
+                    milliseconds=int(expiry_ms.decode()),
+                )
+                db[k] = Value(
+                    value=v,
+                    expiry=now + expiry_ms,
+                )
+                if not is_replica_conn:
+                    conn.send(encode_resp("OK"))
+            case [b"GET", k]:
+                now = datetime.datetime.now()
+                value = db.get(k)
+                if value is None:
+                    conn.send(encode_resp(None))
+                elif value.expiry is not None and now >= value.expiry:
+                    db.pop(k)
+                    conn.send(encode_resp(None))
+                else:
+                    conn.send(encode_resp(value.value))
+            case [b"INFO", b"replication"]:
+                if args.replicaof is None:
+                    info = f"""\
+role:master
+master_replid:{replication.master_replid}
+master_repl_offset:{replication.master_repl_offset}
+""".encode()
+                else:
+                    info = b"""role:slave\n"""
+                conn.send(encode_resp(info))
+            case [b"REPLCONF", b"listening-port", port]:
+                conn.send(encode_resp("OK"))
+            case [b"REPLCONF", b"capa", b"psync2"]:
+                conn.send(encode_resp("OK"))
+            case [b"PSYNC", replid, offset]:
+                conn.send(
+                    encode_resp(
+                        f"FULLRESYNC "
+                        f"{replication.master_replid} "
+                        f"{replication.master_repl_offset}"
+                    )
+                )
+                conn.send(encode_resp(EMPTY_RDB, trailing_crlf=False))
+                replication.connected_replicas.append(conn)
+            case _:
+                raise RuntimeError(f"Command not implemented: {value}")
 
 
-def ping_master_for_repl_conf(s: socket.socket, port: str):
-    add_wait_to_send_message(
-        s, generate_resp_array(["REPLCONF", "listening-port", port]).encode()
+def main(args: Args):
+    server_socket = socket.create_server(
+        ("localhost", args.port),
+        reuse_port=True,
     )
-    add_wait_to_send_message(
-        s, generate_resp_array(["REPLCONF", "capa", "psync2"]).encode()
-    )
+    if args.replicaof is not None:
+        (host, port) = args.replicaof.split(" ")
+        port = int(port)
+        master_conn = socket.create_connection((host, port))
 
+        # Handshake PING
+        master_conn.send(encode_resp([b"PING"]))
+        resp, _ = parse_next(master_conn.recv(4096))
+        assert resp == "PONG"
+        # Handshake REPLCONF listening-port
+        master_conn.send(
+            encode_resp(
+                [
+                    b"REPLCONF",
+                    b"listening-port",
+                    str(args.port).encode(),
+                ]
+            )
+        )
+        resp, _ = parse_next(master_conn.recv(4096))
+        assert resp == "OK"
+        # Handshake REPLCONF capabilities
+        master_conn.send(encode_resp([b"REPLCONF", b"capa", b"psync2"]))
+        resp, _ = parse_next(master_conn.recv(4096))
+        assert resp == "OK"
 
-def ping_master_for_sync(s: socket.socket):
-    add_wait_to_send_message(s, generate_resp_array(["PSYNC", "?", "-1"]).encode())
+        # Handshake PSYNC
+        master_conn.send(encode_resp([b"PSYNC", b"?", b"-1"]))
+        resp, _ = parse_next(master_conn.recv(4096))
+        assert isinstance(resp, str)
+        assert resp.startswith("FULLRESYNC")
+        # Receive db
+        resp_bs = master_conn.recv(4096)
 
+        print(f"Handshake with master completed: {resp=}")
 
-def handshake_with_master(s: socket.socket, port: str):
-    ping_master(s)
-    ping_master_for_repl_conf(s, port)
-    ping_master_for_sync(s)
-
-
-def main(args):
-
-    server_socket = socket.create_server(("localhost", int(args.port)), reuse_port=True)
-    keystore = rdb_parser.read_file_and_construct_kvm(args.dir, args.dbfilename)
-    parse_master_config(args.replicaof)
-
-    if is_replica():
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        s.connect((master_host, int(master_host_port)))
-
-        global master_connection_socket
-        master_connection_socket = s
-
-        handshake_with_master(s, args.port)
-
-    while True:
-        c, (conn_host, _)= server_socket.accept()
         threading.Thread(
-            target=handle_request,
-            args=(
-                c,
-                keystore,
-                args,
-                conn_host
-            ),
+            target=handle_conn,
+            args=(args, master_conn, True),
+            daemon=True,
         ).start()
+    with server_socket:
+        while True:
+            (conn, _) = server_socket.accept()
+            threading.Thread(
+                target=handle_conn,
+                args=(
+                    args,
+                    conn,
+                    False,
+                ),
+                daemon=True,
+            ).start()
 
 
 if __name__ == "__main__":
-    parser.add_argument("--dir", default=".")
-    parser.add_argument("--dbfilename", default="empty.rdb")
-    parser.add_argument("--port", default=6379)
-    parser.add_argument("--replicaof", default="")
-    args = parser.parse_args()
-    main(args)
+    args = argparse.ArgumentParser()
+    args.add_argument("--port", type=int, default=6379)
+    args.add_argument("--replicaof", required=False)
+    main(cast(Args, args.parse_args()))
